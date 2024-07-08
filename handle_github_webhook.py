84df32d7e4743f9a6f1b44cadb016a5ee17df4b4
HippieVibe <none>
2024-07-08 21:06:55 +0200

    Various webhook handler improvements

--- handle_github_webhook.py
+++ handle_github_webhook.py
@@ -34,46 +34,53 @@ def github_webhook() -> ResponseReturnValue:
 
     # Verify signature
     if not hmac.compare_digest(signature, "sha256=" + digest):
-        abort(400, "Invalid signature")
+        abort(400, "invalid signature")
 
     # The signature was fine, let's parse the data
     request_data = request.get_json()
 
     if request_data["action"] != "completed":
-        abort(200, "Not interested in this event")
+        return "not interested in this event"
 
     workflow = request_data["workflow_run"]
 
     if workflow["conclusion"] != "success":
-        abort(200, "Only interested in successful workflows")
+        return "only interested in successful workflows"
 
     branch = workflow["head_branch"]
     artifacts_url = workflow["artifacts_url"]
     target_path = f"{HTML_FOLDER_ROOT}/{branch}"
+
     resp = requests.get(artifacts_url, timeout=HTTP_TIMEOUT)
+
+    archive_download_url = None
     for artifact in resp.json()["artifacts"]:
         if artifact["name"] == "generated_html":
-            with TemporaryDirectory() as tmp_dir:
-                generated_html_zip_path = f"{tmp_dir}/generated_html.zip"
-
-                with requests.get(
-                    artifact["archive_download_url"],
-                    headers={"Authorization": f"Bearer {GITHUB_ACCESS_TOKEN}"},
-                    timeout=HTTP_TIMEOUT,
-                    stream=True,
-                ) as zip_stream:
-                    zip_stream.raise_for_status()
-
-                    with open(generated_html_zip_path, "wb") as zip_file:
-                        for chunk in zip_stream.iter_content(chunk_size=8192):
-                            zip_file.write(chunk)
-
-                    with zipfile.ZipFile(generated_html_zip_path, "r") as zip_ref:
-                        if os.path.isdir(target_path):
-                            shutil.rmtree(target_path)
-                        zip_ref.extractall(target_path)
-
+            archive_download_url = artifact["archive_download_url"]
             break
+
+    if archive_download_url is None:
+        abort(400, "generated HTML artifact not found")
+
+    with TemporaryDirectory() as tmp_dir:
+        generated_html_zip_path = f"{tmp_dir}/generated_html.zip"
+
+        with requests.get(
+            archive_download_url,
+            headers={"Authorization": f"Bearer {GITHUB_ACCESS_TOKEN}"},
+            timeout=HTTP_TIMEOUT,
+            stream=True,
+        ) as zip_stream:
+            zip_stream.raise_for_status()
+
+            with open(generated_html_zip_path, "wb") as zip_file:
+                for chunk in zip_stream.iter_content(chunk_size=8192):
+                    zip_file.write(chunk)
+
+            with zipfile.ZipFile(generated_html_zip_path, "r") as zip_ref:
+                if os.path.isdir(target_path):
+                    shutil.rmtree(target_path)
+                zip_ref.extractall(target_path)
 
     return "done"
 
